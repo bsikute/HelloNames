5e0bea850fc9d667b8ccaf9028028bbc6af106ad
Brian Sikute <brian.sikute@gmail.com>
2019-12-13 19:19:28 +0200

    feature: added favourite cake

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace HelloName
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string fname = "Brian";
+            string lname = "Sikute";
+            string cake = "Carrot";
+            Console.WriteLine("Hello World. My name is {0} {1}", fname, lname);
+            Console.WriteLine("She likes {0} cake", cake);
         }
     }
 }
